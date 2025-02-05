fc466f9f7d453a75295e27b0769f7c4b59a07442
HoriX <112908339+CoderX92@users.noreply.github.com>
2025-02-05 04:13:59 +0200

    Update jquery-3.5.1.min.dc5e7f18c8.js

--- d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js
+++ d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js
@@ -1126,10 +1126,10 @@ __p+='`), E
         }
 
         function g() {
-           var m = e('<a class="w-webflow-badge"></a>').attr("href", "https://webflow.com?utm_campaign=brandjs"),
+           var m = e('<a class="w-webflow-badge"></a>'),
               w = 1 + 2
               }),
-              I = e("<img>").attr("src", "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-text-d2.c82cec3b78.svg").attr("alt", "Made in Webflow");
+              I = e("");
            return m.append(w, I), m[0]
         }
 
